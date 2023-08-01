f02f02dcace58eac784947b4946107042b3f1212
diyorbek <diyorbek02odilov@gmail.com>
2023-08-01 13:55:13 +0500

    added configuration and validation for user

--- Application/UseCases/Users/Command/CreateUser/CreateUserCommandValidator.cs
+++ Application/UseCases/Users/Command/CreateUser/CreateUserCommandValidator.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Application.UseCases.Users.Command.CreateUser;
 
-namespace Application.UseCases.Users.Command.CreateUser
+public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
 {
-    internal class CreateUserCommandValidator
+    public CreateUserCommandValidator()
     {
+        RuleFor(x => x.Email)
+          .NotEmpty().WithMessage("should be not empty value")
+          .Matches("^[a-zA-Z0-9._%+-]+@gmail\\.com$").WithMessage("Email must be in the format exemple@gmail.com");
+
+        RuleFor(x => x.PhoneNumber)
+          .NotEmpty().WithMessage("should be not empty value")
+          .Matches(@"^\+998(9[0-9])\d{7}$").WithMessage("PhoneNumber should be in the format +99891 234 56 78");
+
+        RuleFor(x => x.FullName).NotEmpty()
+           .WithMessage("should be not empty value")
+           .MaximumLength(50).WithMessage("fullName must have a maximum length of 50 characters");
+
+        RuleFor(x => x.PasswordHash)
+            .NotEmpty().WithMessage("should be not empty value")
+            .Matches("@\"^(?=.*[a-zA-Z]{4,})(?=.*\\d{4,}).{8,}$\";")
+                 .WithMessage("Password must contain at least 4 letters and 4 numbers");
+
+        RuleFor(x => x.ConfirmPassword).Equal("PasswordHash").WithMessage("confirmpassword is not homogeneous");
     }
 }
--- Infrastructure/Persistance/Configurations/UserConfiguration.cs
+++ Infrastructure/Persistance/Configurations/UserConfiguration.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Application.Comman.Extentions;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Infrastructure.Persistance.Configuration
+namespace Infrastructure.Persistance.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
 {
-    internal class UserConfiguration
+    public void Configure(EntityTypeBuilder<User> builder)
     {
+        builder.Property(x => x.Id).IsRequired();
+        builder.HasIndex(x => x.Id).IsUnique();
+        builder.Property(x => x.Email).IsRequired();
+        builder.HasIndex(x => x.Email).IsUnique();
+        builder.Property(x => x.PhoneNumber).IsRequired();
+        builder.HasIndex(x => x.PhoneNumber).IsUnique();
+        builder.Property(x => x.FullName).IsRequired().HasMaxLength(50);
+        builder.HasData(new User()
+        {
+            FullName = "Diyorbek",
+            Email = "diyorbek02odilov@gmail.com",
+            PhoneNumber = "+998942922288",
+            PasswordHash = "diyorbek020819".stringHash(),
+        });
     }
 }
