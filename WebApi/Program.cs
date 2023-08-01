126e83830c08fbd3208d68a22e96b2472c627d75
diyorbek <diyorbek02odilov@gmail.com>
2023-08-01 17:07:00 +0500

    updated configureService

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,39 +1,38 @@
 using Application;
 using Infrastructure;
-namespace WebApi
+namespace WebApi;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        var configuration = builder.Configuration;
+
+        builder.Services.AddInfrastructureServices(configuration);
+        builder.Services.AddApplicationServices(configuration);
+        builder.Services.AddWebUIServices(configuration);
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            var configuration = builder.Configuration;
-
-            builder.Services.AddInfrastructureServices(configuration);
-            builder.Services.AddApplicationServices(configuration);
-            builder.Services.AddWebUIServices(configuration);
-
-            var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-            app.UseGlobalExceptionMiddleware();
-
-
-            app.MapControllers();
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+        app.UseGlobalExceptionMiddleware();
+
+
+        app.MapControllers();
+        app.Run();
     }
 }
